9442035bc7af88ea4ca8d65cb0180163c9799595
kthelab <keventavares.kdgt@gmail.com>
2026-03-28 17:56:06 -0300

    batman

---
+++ cmd/tcplistener/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"gottp/internal/request"
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("error", "error", err)
+		}
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("error", "error", err)
+		}
+
+		fmt.Printf("Request line: \n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
+
+	}
+}
