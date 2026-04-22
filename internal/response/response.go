fd6bc375280a468ff45974b1d856e51c36e2e6d7
kthelab <keventavares.kdgt@gmail.com>
2026-04-21 23:27:55 -0300

    response fixes

--- internal/response/response.go
+++ internal/response/response.go
@@ -44,11 +44,11 @@ func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
 
 	switch statusCode {
 	case StatusOk:
-		statusLine = []byte("HTTP/1.1 200 OK")
+		statusLine = []byte("HTTP/1.1 200 OK\r\n")
 	case StatusBadRequest:
-		statusLine = []byte("HTTP/1.1 400 Bad Request")
+		statusLine = []byte("HTTP/1.1 400 Bad Request\r\n")
 	case StatusInternalServerError:
-		statusLine = []byte("HTTP/1.1 500 Internal Server Error")
+		statusLine = []byte("HTTP/1.1 500 Internal Server Error\r\n")
 	default:
 		return fmt.Errorf("unrecognized error code")
 	}
