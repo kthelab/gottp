a56fd2bbe4876945cca00de4e2c34e319c7352b4
kthelab <keventavares.kdgt@gmail.com>
2026-04-27 17:41:31 -0300

    chuncked responses

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -6,8 +6,10 @@ import (
 	"gottp/internal/response"
 	"gottp/internal/server"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 )
 
@@ -59,14 +61,42 @@ func main() {
 			body = respond400()
 			status = response.StatusBadRequest
 
-		}
-		if req.RequestLine.RequestTarget == "/myproblem" {
+		} else if req.RequestLine.RequestTarget == "/myproblem" {
 			body = respond500()
 			status = response.StatusInternalServerError
+
+		} else if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin/stream") {
+			target := req.RequestLine.RequestTarget
+			res, err := http.Get("https://httpbin.org/" + target[len("/httpbin/"):])
+			if err != nil {
+				body = respond500()
+				status = response.StatusInternalServerError
+			} else {
+				w.WriteStatusLine(status)
+
+				h.Delete("Content-Length")
+				h.Set("Transfer-Encoding", "chunked")
+				h.Replace("Content-Type", "text/plain")
+				w.WriteHeaders(*h)
+
+				for {
+					data := make([]byte, 32)
+					n, err := res.Body.Read(data)
+					if err != nil {
+						break
+					}
+
+					w.WriteBody([]byte(fmt.Sprintf("%x\r\n", n)))
+					w.WriteBody(data[:n])
+					w.WriteBody([]byte("\r\n"))
+				}
+				w.WriteBody([]byte("0\r\n\r\n"))
+				return
+			}
 		}
 
-		h.Replace("content-Length", fmt.Sprintf("%d", len(body)))
-		h.Replace("content-Type", "text/html")
+		h.Replace("Content-Length", fmt.Sprintf("%d", len(body)))
+		h.Replace("Content-Type", "text/html")
 		w.WriteStatusLine(status)
 		w.WriteHeaders(*h)
 		w.WriteBody(body)
